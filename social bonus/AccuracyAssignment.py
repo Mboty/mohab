8a2af7859bea67612ef289e667944ded39654ad3
Mohab Mohamed <155490979+Mboty@users.noreply.github.com>
2025-11-26 22:55:51 +0200

    Add files via upload

---
+++ social bonus/AccuracyAssignment.py
@@ -0,0 +1,38 @@
+import torch
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+data = dataset[0]
+
+data.x = F.normalize(data.x, p=1, dim=1)
+
+class SimpleGCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = GCNConv(data.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
+
+model = SimpleGCN()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+for epoch in range(300):
+    model.train()
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+model.eval()
+pred = model(data).argmax(dim=1)
+acc = (pred[data.test_mask] == data.y[data.test_mask]).float().mean()
+print(f'GCN Test Accuracy: {acc:.2f}')
